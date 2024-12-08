4f61e744883651af506423da6a296152c6787332
tammim28 <tammimhasan123@gmail.com>
2024-12-07 21:18:32 -0800

    complete add money and cash out features

---
+++ js/cashout.js
@@ -0,0 +1,17 @@
+document.getElementById('btn-cash-out').addEventListener('click', function(event){
+    event.preventDefault();
+    const cashOut = document.getElementById('input-cash-out').value;
+    const cashOutNumber = parseFloat(cashOut);
+    const pinNumber = document.getElementById('input-cash-out-pin').value;
+    console.log(cashOut, pinNumber);
+
+    if(pinNumber=== '1234'){
+        const balance = document.getElementById('acount-balance').innerText;
+        const balanceNumber = parseFloat(balance);
+        const newBalance = balanceNumber - cashOutNumber;
+        document.getElementById('acount-balance').innerText = newBalance;
+    }
+    else{
+        alert('faild to cash out');
+    }
+})
